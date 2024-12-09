3c64cb01fef78f827c54f0e2b2c3d15be9ea7c88
Max Masterius <1058433+rxqd@users.noreply.github.com>
2024-12-10 00:41:50 +0200

    Parsed parens

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::env;
 use std::fs;
-use std::io::{self, Write};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
-        writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
+        eprintln!("Usage: {} tokenize <filename>", args[0]);
         return;
     }
 
@@ -14,21 +13,52 @@ fn main() {
 
     match command.as_str() {
         "tokenize" => {
-            let code_lines = fs::read_to_string(filename).
-                unwrap_or_else(|_| {
-                    writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
-                    String::new()
-                });
+            let code_lines = fs::read_to_string(filename).unwrap_or_else(|_| {
+                eprintln!("Failed to read file {}", filename);
+                String::new()
+            });
 
-            if !code_lines.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+            if code_lines.is_empty() {
+                println!("EOF  null");
+                return;
             }
+
+            scan_code(&code_lines);
         }
         _ => {
-            writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return;
+            eprintln!("Unknown command: {}", command);
         }
     }
+}
+
+fn scan_code(code_lines: &str) {
+    let mut parsed_lines: Vec<String> = code_lines.split('\n').map(scan_code_line).collect();
+
+    parsed_lines.push("EOF  null".to_owned());
+
+    for line in parsed_lines {
+        println!("{line}");
+    }
+}
+
+fn scan_code_line(line: &str) -> String {
+    let mut result = String::new();
+
+    for c in line.chars() {
+        match c {
+            '(' => result.push_str("LEFT_PAREN ( null"),
+            ')' => result.push_str("RIGHT_PAREN ) null"),
+            '+' => result.push_str("PLUS + null"),
+            '-' => result.push_str("MINUS - null"),
+            '*' => result.push_str("MULTIPLY * null"),
+            '/' => result.push_str("DIVIDE / null"),
+            '=' => result.push_str("EQUAL = null"),
+            ' ' | '\t' => {}
+            _ => result.push_str(&format!("UNKNOWN {} null ", c)),
+        }
+
+        result.push('\n');
+    }
+
+    result.trim().to_owned()
 }
